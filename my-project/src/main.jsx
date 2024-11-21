07c6dafba7afef07de8f0c2370c9140efe4b9c3c
Thyale MD <thyaledantas@gmail.com>
2024-11-21 11:41:29 -0300

    Sessão sobre o fotografo finalizada

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './Header'
+import Sobre from './Main/Sobre'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
+    <Sobre/>
   </StrictMode>,
 )
