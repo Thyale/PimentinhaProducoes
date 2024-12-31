0b894a6bb6104911b13f5b1fb05506acd7a891ec
Luiz Antônio Valeriano <144851774+Thyale@users.noreply.github.com>
2024-12-31 16:49:42 -0300

    Update Whats.jsx

--- my-project/src/Main/Whats.jsx
+++ my-project/src/Main/Whats.jsx
@@ -1,7 +1,7 @@
 export default function Whats(){
     return(
         <a target="_blank" href="https://wa.me/5581999641204?text=Olá,%20gostaria%20de%20mais%20informações!">
-          <i className="fab fa-whatsapp bg-green-700 text-white py-3 px-4 text-4xl rounded-full fixed bottom-4 right-4"></i>
+          <i className="fab fa-whatsapp bg-green-700 text-white py-3 px-4 text-4xl rounded-full z-40 fixed bottom-4 right-4"></i>
         </a>
 
     )
