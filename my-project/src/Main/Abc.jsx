1b161bec7b19f9b2fdcabb4482801768f427d72c
Luiz Antônio Valeriano <thyaledantas@gmail.com>
2024-12-31 14:50:06 -0300

    Update Abc.jsx

--- my-project/src/Main/Abc.jsx
+++ my-project/src/Main/Abc.jsx
@@ -24,17 +24,17 @@ export default function Abc(){
                 </div>
                 <div className="flex flex-row items-center flex-wrap w-full justify-around mt-6 gap-3">
                     <div id="FotosAbc" className="w-full max-w-96">
-                        <img className="shadow-lg" src="/PraiaAbcImg.png" alt="Imagem de sessão de fotos na praia" />
+                        <img className="shadow-lg" src="/public/PraiaAbcImg.png" alt="Imagem de sessão de fotos na praia" />
                         <h1 className="font-aboreto text-2xl mt-3">FOTOS NA PRAIA</h1>
                         <a target="_blank" className="block w-full bg-Amarelo font-aboreto hover:bg-hoverAmarelo mt-3 text-Branco rounded-md text-center py-1" href="https://youtu.be/1guQOSwTC9Q?si=OeDCd2N26a6114du">Assistir Vídeo</a>
                     </div>
                     <div id="FotosAbc" className="w-full max-w-96">
-                        <img className="shadow-lg" src="/BecaAbcImg.png" alt="Imagem de sessão de fotos com beca" />
+                        <img className="shadow-lg" src="/public/BecaAbcImg.png" alt="Imagem de sessão de fotos com beca" />
                         <h1 className="font-aboreto text-2xl mt-3">FOTOS DE BECA</h1>
                         <a className="bg-Verde font-aboreto block w-full hover:bg-hoverVerde mt-3 text-white rounded-md text-center py-1" target="_blanck" href="https://www.instagram.com/reel/DAPVGoCOI7x/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">Assistir Vídeo</a>
                     </div>
                     <div id="FotosAbc" className="w-full max-w-96">
-                        <img className="shadow-lg" src="/FormaturaAbcImg.jpg" alt="Imagem de sessão de fotos da festa de formatura" />
+                        <img className="shadow-lg" src="/public/FormaturaAbcImg.jpg" alt="Imagem de sessão de fotos da festa de formatura" />
                         <h1 className="font-aboreto text-2xl mt-3">FESTA DE FORMATURA</h1>
                         <a className="bg-Salmao block font-aboreto hover:bg-hoverSalmao w-full mt-3 py-1 text-center text-white rounded-md" target="_blanck" href="https://www.facebook.com/100001988834996/videos/430622826325666/">Assistir Vídeo</a>
                     </div>
